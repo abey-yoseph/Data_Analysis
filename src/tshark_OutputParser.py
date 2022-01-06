cf41a2c2c4890880444a6c7d053d835a5bf642fb
Abey <abey.yoseph@leidos.com>
2022-01-05 20:30:16 -0500

    error handling for tsharkOutputParser

--- src/tshark_OutputParser.py
+++ src/tshark_OutputParser.py
@@ -3,6 +3,7 @@ import sys
 import pandas as pd
 from csv import writer
 from csv import reader
+import math
 
 inFile = sys.argv[1]
 fileName = inFile.split(".")
@@ -14,25 +15,28 @@ bsm_substr = "0014"
 
 #searches for one of the three substrings in the extracted tshark data and returns the J2735 message
 def getPayload(row):
-    if map_substr in row[1]:
-        map_index = row[1].find(map_substr)
-        map = row[1][map_index:]
+    try:
+        if map_substr in row[1]:
+            map_index = row[1].find(map_substr)
+            map = row[1][map_index:]
 
-        return map
-    elif spat_substr in row[1]:
-        spat_index = row[1].find(spat_substr)
-        spat_length = len(row[1])
-        if "mk5" in fileName[0] and "rx" in fileName[0]:
-            spat = row[1][spat_index:spat_length-8]
-        else:
-            spat = row[1][spat_index:]
+            return map
+        elif spat_substr in row[1]:
+            spat_index = row[1].find(spat_substr)
+            spat_length = len(row[1])
+            if "mk5" in fileName[0] and "rx" in fileName[0]:
+                spat = row[1][spat_index:spat_length-8]
+            else:
+                spat = row[1][spat_index:]
 
-        return spat
-    elif bsm_substr in row[1]:
-        bsm_index = row[1].find(bsm_substr)
-        bsm = row[1][bsm_index:]
+            return spat
+        elif bsm_substr in row[1]:
+            bsm_index = row[1].find(bsm_substr)
+            bsm = row[1][bsm_index:]
 
-        return bsm
+            return bsm
+    except:
+        print("Error getting payload for: " + str(row[1]))
 
 
 #read the tshark file and create a file containing timestamps and payloads
